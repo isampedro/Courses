badfd020232ed0f39d2451c895b4b7e3ab345479
Ignacio <isampedro@itba.edu.ar>
2021-10-19 18:01:01 -0300

    currification 2.0

--- haskell/FP Exercises/currification.hs
+++ haskell/FP Exercises/currification.hs
@@ -1,3 +1,5 @@
+import Data.Tuple
+
 curry :: ((a, b) -> c) -> a -> b -> c
 curry f x y = f (x, y)
 
@@ -12,3 +14,15 @@ twice f = f . f
 
 id :: a -> a
 id x = x
+
+flip :: (a -> a -> b) -> a -> a -> b
+flip f x y = f y x
+
+uflip :: ((b, a) -> c) -> (a, b) -> c
+uflip f = f . swap
+
+const :: a -> a -> a
+const x _ = x
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g = f . g
